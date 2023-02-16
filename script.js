d21fec67f38f34f8f311d424f5b31cb241d32d87
lucas.witt <lucas.witt.89@gmail.com>
2023-02-16 11:29:29 +0100

    Se agrego el proyecto

---
+++ script.js
@@ -0,0 +1,50 @@
+ //e -- enter
+ //o -- ober 
+ //i -- imes 
+ //a -- ai 
+ //u -- ufat
+
+
+ function Encriptar() {
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    //i es para que afecte mayusculas y minuscualas
+    // g es para toda la linea y oracion
+    // m es para que afecte a multiples lineas o parrafos
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+    var txtCifrado = txtCifrado.replace(/i/igm,"imes");
+    var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+    var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+ }
+
+ function Desencriptar(){
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    //i es para que afecte mayusculas y minuscualas
+    // g es para toda la linea y oracion
+    // m es para que afecte a multiples lineas o parrafos
+    var txtCifrado = texto.replace(/enter/igm,"e");
+    var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+    var txtCifrado = txtCifrado.replace(/imes/igm,"i");
+    var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+   }
+
+   function Copy(){
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand("cut");
+    alert("El texto se copió!");
+   }
+
